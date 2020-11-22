d4a2d86f24dde5263aa5838111ef0b4c54188da3
mayankraj30 <mayankrajaraj@gmail.com>
2020-11-22 13:04:22 +0530

    components part 2 implimenting onclick and setstate and props

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,9 +3,21 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
-import Menu from './components/MenuComponent'
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+      dishes : DISHES
+      
+    };
+  }
   render() {
     return (
       <div>
@@ -14,7 +26,7 @@ class App extends Component {
             <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes = {this.state.dishes}/>
 
       </div>
     );
